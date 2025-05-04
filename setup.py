a0783602d6f3262e185c04d796360233c569ead0
Selin Ceydeli <s.ceydeli@student.tudelft.nl>
2025-05-04 10:08:50 +0200

    release with automatic versioning is setup

--- setup.py
+++ setup.py
@@ -1,13 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(
-    name="libml",
-    version="0.1",
-    packages=find_packages(),
-    install_requires=[
-        "nltk",
-        "scikit-learn",
-    ],
-    description="Preprocessing logic for the restaurant sentiment model",
-    url="https://github.com/remla25-team12/lib-ml",
-)
+setup()
